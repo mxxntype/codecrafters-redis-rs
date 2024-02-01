53586a459c5b3dfeb0afcb30dff40268c86f153a
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-02-01 18:34:00 +0300

    feat: Respond to multiple inline `PING` requests bug: Hangs after handling the first client

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
-use std::{io::Write, net::TcpListener};
+//! In this challenge, you'll build a toy Redis clone that's capable of handling
+//! basic commands like `PING`, `SET` and `GET`. Along the way we'll learn about
+//! event loops, the Redis protocol and more.
+//!
+//! **Note**: If you're viewing this repo on GitHub, head over to
+//! [codecrafters.io](https://codecrafters.io) to try the challenge.
+
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
 
 const PONG_RESPONSE: &[u8] = b"+PONG\r\n";
 
@@ -9,11 +19,21 @@ fn main() -> anyhow::Result<()> {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                let _ = stream.write(PONG_RESPONSE)?;
+                handle_client(&mut stream)?;
             }
             Err(e) => println!("error accepting connection: {}", e),
         }
     }
 
+    Ok(())
+}
+
+// FIXME: Handles a single request like "ping\nping\nping" fine,
+// but any following requests hang?..
+fn handle_client(stream: &mut TcpStream) -> anyhow::Result<()> {
+    let mut buf = [0u8; 20];
+    while let Ok(_read_bytes) = stream.read(&mut buf) {
+        _ = stream.write(PONG_RESPONSE);
+    }
     Ok(())
 }
