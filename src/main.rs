77558b4e10f8b9963f6746faf681050c2bf7dc68
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-02-01 19:13:48 +0300

    refactor: Use a `tokio`-based event loop for handling clients

--- src/main.rs
+++ src/main.rs
@@ -5,35 +5,29 @@
 //! **Note**: If you're viewing this repo on GitHub, head over to
 //! [codecrafters.io](https://codecrafters.io) to try the challenge.
 
-use std::{
-    io::{Read, Write},
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
 };
 
 const PONG_RESPONSE: &[u8] = b"+PONG\r\n";
 
-fn main() -> anyhow::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:6379")?;
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                std::thread::spawn(move || handle_client(&mut stream).unwrap());
-            }
-            Err(e) => println!("error accepting connection: {}", e),
-        }
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+        handle_client(&mut socket).await?;
     }
-
-    Ok(())
 }
 
-fn handle_client(stream: &mut TcpStream) -> anyhow::Result<()> {
+async fn handle_client(stream: &mut TcpStream) -> anyhow::Result<()> {
     let mut request = [0u8; 32];
 
     // `stream.read()` reads until a newline, so lets
     // run it in a loop to read everything line-by-line.
-    while let Ok(read_bytes) = stream.read(&mut request) {
+    while let Ok(read_bytes) = stream.read(&mut request).await {
         // Having nothing to read is not an error, it's an Ok(0).
         // Without this, the loop will run until an error occurs.
         if read_bytes == 0 {
@@ -41,7 +35,7 @@ fn handle_client(stream: &mut TcpStream) -> anyhow::Result<()> {
         }
 
         // If we actually read something meaningful, respond to it.
-        _ = stream.write(PONG_RESPONSE)?;
+        _ = stream.write(PONG_RESPONSE).await?;
     }
 
     Ok(())
