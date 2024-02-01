60b6b1abe2c3720c2153d45fa78569a748360fcc
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-02-01 17:09:47 +0300

    feat: Respond to all requests as if they are `PING` requests

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() -> anyhow::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379")?;
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => println!("accepted new connection"),
+            Ok(mut stream) => {
+                println!("accepted new connection");
+                stream.write(b"+PONG\r\n")?;
+            }
             Err(e) => println!("error: {}", e),
         }
     }
