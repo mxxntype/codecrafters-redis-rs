6be37732aab90908ef0378c5289ab79865f4d203
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-02-01 17:02:01 +0300

    feat: Listen on `TCP:6379`, return `anyhow::Result` from `main`

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,14 @@
-// Uncomment this block to pass the first stage
-// use std::net::TcpListener;
+use std::net::TcpListener;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+fn main() -> anyhow::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379")?;
 
-    // Uncomment this block to pass the first stage
-    //
-    // let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_stream) => println!("accepted new connection"),
+            Err(e) => println!("error: {}", e),
+        }
+    }
+
+    Ok(())
 }
